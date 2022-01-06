95ac4530d915cba88aa945e692c0b635ff9c94e1
GitHub Action <action@github.com>
2022-01-06 03:32:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/nathanewhite/locoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nathanewhite/locoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nathanewhite.locoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nathanewhite.locoplugin.MyBundle
 
 class MyApplicationService {
 
